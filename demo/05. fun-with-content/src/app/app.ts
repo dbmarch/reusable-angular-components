28e0b7e6ac30d266e65d670123c0cb32ed8d6876
Kobi Hari <kobi2294@yahoo.com>
2026-03-05 13:32:37 +0200

    S05L09 - Using custom properties to style components

--- demo/05. fun-with-content/src/app/app.ts
+++ demo/05. fun-with-content/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { Icon } from './components/icon/icon';
 import { Expander } from './components/expander/expander';
-import { Blank } from './components/blank/blank';
 
 @Component({
   selector: 'app-root',
-  imports: [Expander, Icon, Blank],
+  imports: [Expander, Icon],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
