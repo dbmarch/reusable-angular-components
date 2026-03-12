3a8ce8588cdcaf0f3d0b34198323280101117665
Kobi Hari <kobi2294@yahoo.com>
2026-03-12 19:37:10 +0200

    S07L08 - Template Context Type Safety

--- demo/07. fun-with-templates/src/app/shared/components/item-selector/item-template.directive.ts
+++ demo/07. fun-with-templates/src/app/shared/components/item-selector/item-template.directive.ts
@@ -1,8 +1,16 @@
 import { Directive, inject, TemplateRef } from "@angular/core";
 
+export interface ItemTemplateContext {
+    readonly $implicit: string;
+}
+
 @Directive({
     selector: '[appItemTemplate]'
 })
 export class ItemTemplateDirective {
-    readonly template = inject(TemplateRef<any>);
+    readonly template = inject(TemplateRef<ItemTemplateContext>);
+
+    static ngTemplateContextGuard(_: ItemTemplateDirective, ctx: unknown): ctx is ItemTemplateContext {
+        return true;
+    }
 }
