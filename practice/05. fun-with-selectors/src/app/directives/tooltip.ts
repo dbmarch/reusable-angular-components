72aa659192477a336f187553de48fa1fa71baffb
Dave March <dbmarch@gmail.com>
2026-04-25 22:47:14 -0400

    Use inputs instead

--- practice/05. fun-with-selectors/src/app/directives/tooltip.ts
+++ practice/05. fun-with-selectors/src/app/directives/tooltip.ts
@@ -1,17 +1,25 @@
-import { Directive, effect, ElementRef, inject, signal } from '@angular/core';
+import { Directive, effect, ElementRef, inject, input, signal } from '@angular/core';
 
 @Directive({
-  selector: 'img',
+  selector: 'img[alt]',
   host: {
-    '[title]': 'title()',
-    '(mouseenter)': 'onImage()',
+    '[title]': 'alt()',
+    // '(mouseenter)': 'onImage()',
   },
 })
 export class Tooltip {
-  private el = inject(ElementRef);
+  // Read the alt attribute of the image and set it as the title when the mouse enters the image
+  // private el = inject(ElementRef);
   title = signal('');
+  readonly alt = input('');
 
-  onImage() {
-    this.title.set(this.el.nativeElement.getAttribute('alt'));
-  }
+  // onImage() {
+  //   this.title.set(this.el.nativeElement.getAttribute('alt'));
+  // }
+
+  // constructor() {
+  //   effect(() => {
+  //     console.log(`Tooltip: ${this.alt()}`);
+  //   });
+  // }
 }
