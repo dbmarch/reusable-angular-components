920d68f44c125b3a6494e2939f45581a8ebce796
Dave March <dbmarch@gmail.com>
2026-04-30 00:30:19 -0400

    replace module

--- practice/09. fun-with-content-inject-button/src/app/app.ts
+++ practice/09. fun-with-content-inject-button/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Icon } from './components/icon/icon';
-import { ExpanderModule } from './components/expander/expander.module';
+// import { ExpanderModule } from './components/expander/expander.module.ts.old';
+import { Expander } from './components/expander/expander';
 
 @Component({
   selector: 'app-root',
-  imports: [ExpanderModule, Icon],
+  imports: [Expander, Icon],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- practice/09. fun-with-content-inject-button/src/app/components/expander/expander.ts
+++ practice/09. fun-with-content-inject-button/src/app/components/expander/expander.ts
@@ -1,22 +1,4 @@
-import { Component, computed, input, signal } from '@angular/core';
+import { ExpanderToggleDirective } from './expander-toggle/expander-toggle';
+import { ExpanderComponent } from './expander.component';
 
-@Component({
-  selector: 'app-expander',
-  imports: [],
-  templateUrl: './expander.html',
-  styleUrl: './expander.scss',
-  host: {
-    '[class.collapsed]': 'isCollapsed()',
-    '[class.expanded]': 'isExpanded()',
-  },
-})
-export class ExpanderComponent {
-  readonly #isExpanded = signal(false);
-
-  readonly isExpanded = this.#isExpanded.asReadonly();
-  readonly isCollapsed = computed(() => !this.isExpanded());
-
-  toggle() {
-    this.#isExpanded.update((v) => !v);
-  }
-}
+export const Expander = [ExpanderComponent, ExpanderToggleDirective];
