0816a245da35e526297978b3359d4b9f5ace1160
Dave March <dbmarch@gmail.com>
2026-04-30 00:04:06 -0400

    attribute content

--- practice/08. fun-with-content-multi-slot/src/app/app.ts
+++ practice/08. fun-with-content-multi-slot/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { ExpanderComponent } from './components/expander/expander';
+import { Icon } from './components/icon/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [ExpanderComponent],
+  imports: [ExpanderComponent, Icon],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-}
+export class App {}
