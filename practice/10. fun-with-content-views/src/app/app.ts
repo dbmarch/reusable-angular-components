8e9a834bd2cb2f39031d79f1af4e330405881469
Dave March <dbmarch@gmail.com>
2026-05-10 23:03:02 -0400

    UPdates

--- practice/10. fun-with-content-views/src/app/app.ts
+++ practice/10. fun-with-content-views/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Icon } from './components/icon/icon';
 import { Expander } from './components/expander/expander';
+import { Blank } from './components/blank/blank';
 
 @Component({
   selector: 'app-root',
-  imports: [Expander, Icon],
+  imports: [Expander, Icon, Blank],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-}
+export class App {}
---
+++ practice/10. fun-with-content-views/src/app/components/blank/blank.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blank',
+  imports: [],
+  templateUrl: './blank.html',
+  styleUrl: './blank.scss',
+})
+export class Blank {
+
+}
